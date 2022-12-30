c736687cddf5daa6d6015078c17140c237a0a8e0
aboveStars <yunuskorkmazis384@outlook.com>
2022-12-30 12:44:38 +0300

    Lesson-7 Done. We are on 12:33:01

--- hardhat-fund-me/scripts/withdraw.js
+++ hardhat-fund-me/scripts/withdraw.js
@@ -4,11 +4,9 @@ async function main() {
     const { deployer } = await getNamedAccounts()
     const fundMe = await ethers.getContract("FundMe", deployer)
     console.log("Funding Contract...")
-    const transactionResponse = await fundMe.fund({
-        value: ethers.utils.parseEther("0.1"),
-    })
+    const transactionResponse = await fundMe.withDraw()
     await transactionResponse.wait(1)
-    console.log("Funded...")
+    console.log("withdrawed...")
 }
 
 main()
