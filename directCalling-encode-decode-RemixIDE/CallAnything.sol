fa3729b4a2891cb54c13b5b66a6fd36c04ba5a6e
aboveStars <yunuskorkmazis384@outlook.com>
2023-01-15 17:47:42 +0300

    Lesson-14 (NFT) Sixth-Part-Done. We are on 22:56:56

---
+++ directCalling-encode-decode-RemixIDE/CallAnything.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CallAnything {
+    address public s_someAdress;
+    uint256 public s_amount;
+
+    function transfer(address someAddress, uint256 amount) public {
+        s_someAdress = someAddress;
+        s_amount = amount;
+    }
+
+    function getSelectorOne() public pure returns (bytes4 selector) {
+        selector = bytes4(keccak256("transfer(address,uint256)"));
+    }
+
+    function getDataToCallTransfer(
+        address someAddress,
+        uint256 amount
+    ) public pure returns (bytes memory) {
+        return abi.encodeWithSelector(getSelectorOne(), someAddress, amount);
+    }
+
+    function callTransferFuntionDirectly(
+        address someAddress,
+        uint256 amount
+    ) public returns (bytes4, bool) {
+        (bool success, bytes memory returnData) = address(this).call(
+            getDataToCallTransfer(someAddress, amount)
+        );
+        return (bytes4(returnData), success);
+    }
+}
---
+++ directCalling-encode-decode-RemixIDE/Encoding.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Encoding {
+    function combineString() public pure returns (string memory) {
+        return string(abi.encodePacked("Hi Mom ", "miss you !"));
+    }
+
+    function encodeNumber() public pure returns (bytes memory) {
+        bytes memory number = abi.encode(1);
+        return number;
+    }
+
+    function encodeString() public pure returns (bytes memory) {
+        bytes memory someString = abi.encode("some string");
+        return someString;
+    }
+
+    function encodeStringpacked()
+        public
+        pure
+        returns (bytes memory someString)
+    {
+        someString = abi.encodePacked("some string");
+    }
+
+    function encodeStringBytes() public pure returns (bytes memory someString) {
+        someString = bytes("some string");
+    }
+
+    function decodeString() public pure returns (string memory) {
+        string memory someString = abi.decode(encodeString(), (string));
+        return someString;
+    }
+
+    function multiEncode() public pure returns (bytes memory) {
+        bytes memory someString = abi.encode("some string", "its bigger!");
+        return someString;
+    }
+
+    function multiDecode() public pure returns (string memory, string memory) {
+        (string memory someString, string memory someOtherString) = abi.decode(
+            multiEncode(),
+            (string, string)
+        );
+        return (someString, someOtherString);
+    }
+
+    function multiEncodePacked() public pure returns (bytes memory) {
+        bytes memory someString = abi.encodePacked("some string", "its bigger");
+        return someString;
+    }
+
+    // this data packed how can I decode this ?
+    function multiDecodePacked() public pure returns (string memory) {
+        string memory someString = abi.decode(multiEncodePacked(), (string));
+        return someString;
+    }
+
+    function multiStringCastPacked() public pure returns (string memory) {
+        string memory someString = string(multiEncodePacked());
+        return someString;
+    }
+}
